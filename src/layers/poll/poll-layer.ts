f6c8f0c0be1906a28163581f26cc8a165b41c342
gzaripov <grigory.zaripov@gmail.com>
2020-05-18 12:10:05 +0300

    chore(layers): remove defaults for poll layer

--- src/layers/poll/poll-layer.ts
+++ src/layers/poll/poll-layer.ts
@@ -2,9 +2,9 @@ import { AxiosLayer } from '../../wrap';
 
 const sleep = (time: number) => new Promise((resolve) => setTimeout(resolve, time));
 
-type Options = { delay?: number; retries?: number };
+type Options = { delay: number; retries: number };
 
-const pollLayer = ({ delay = 2000, retries = 10 }: Options = {}): AxiosLayer => async (request) => {
+const pollLayer = ({ delay, retries }: Options): AxiosLayer => async (request) => {
   const poll = (retry: number): Promise<any> => {
     return request().catch((error) => {
       const retriesLeft = retry - 1;
